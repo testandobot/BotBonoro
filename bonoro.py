719d0f25c16953856ac7ee0f0e0e7841bb1c30b8
testandobot <68176907+testandobot@users.noreply.github.com>
2020-07-17 12:30:28 -0300

    Update bonoro.py

--- bonoro.py
+++ bonoro.py
@@ -14,26 +14,25 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 indice = False
 textos = ["Manda salve pro 3s2, Presidente @jairbolsonaro\nSomos fãs do mito", "Somos muito fãs do excelentíssimo Presidente @jairbolsonaro\nManda salve pro 3S2, mito"]
-print(api.me())
-class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        global indice
-        if status.user.screen_name == "jairbolsonaro" and not "RT" in status.text:
-            try:
-                if not indice:
-                    api.update_status(textos[0], in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
-                else:
-                    api.update_status(textos[1], in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
-                api.create_favorite(status.id)
-                api.retweet(status.id)
-                indice = not indice
-            except tweepy.TweepError as e:
-                print(e.reason)        
-
-
+print(api.me())        
                     
 if __name__ == "__main__":
     while True:
+        class MyStreamListener(tweepy.StreamListener):
+            def on_status(self, status):
+                global indice
+                if status.user.screen_name == "jairbolsonaro" and not "RT" in status.text:
+                    try:
+                        if not indice:
+                            api.update_status(textos[0], in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)                  
+                        else:
+                            api.update_status(textos[1], in_reply_to_status_id=status.id, auto_populate_reply_metadata=True)
+                        api.create_favorite(status.id)
+                        api.retweet(status.id)
+                        indice = not indice
+                    except tweepy.TweepError as e:
+                        print(e.reason)
+            
         myStreamListener = MyStreamListener()
         myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
         myStream.filter(follow=["128372940"])
